afbbb8979f928f36cd289b83f1f6f5544edb0f81
aleja8818 <alejahc_8818@hotmail.com>
2022-11-29 16:42:34 -0500

    se crea proyecto

---
+++ src/Home/Home.js
@@ -0,0 +1,82 @@
+import "./Home.css";
+export function Home() {
+  return (
+    <>
+      <section>
+        <div class="banner"></div>
+      </section>
+      <section>
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-md-8">
+              <h2>Slipknot</h2>
+              <p>
+                Slipknot es una banda estadounidense de metal alternativo
+                formada en 1995 en Des Moines, Iowa, Estados Unidos. Sus
+                integrantes en la actualidad son Corey Taylor, Craig Jones, Jim
+                Root, Mick Thomson, Shawn Crahan y Sid Wilson. Slipknot es
+                conocida por las máscaras características de cada uno de sus
+                miembros, que cambian conforme han sacado más discografía. Sus
+                miembros utilizan dos tipos de guitarra (principal y rítmica),
+                un bajo, dos instrumentos de percusión personalizados, una
+                batería e instrumentos electrónicos como sampler o mesas de
+                mezclas. El sonido de la banda ha sido descrito como "una
+                máquina trilladora devorando un grupo de tambores militares".5​
+                El vocalista Corey Taylor también incorpora diversos estilos
+                vocales, como por ejemplo la voz gutural, el canto melódico y el
+                rapeo, mientras que las letras en general tienen un tono
+                agresivo. Las influencias de la banda incluyen, entre otros
+                géneros, el death metal, el thrash metal y el heavy metal,
+                aunque suelen ser incluidos con asiduidad dentro del metal
+                alternativo y el nu metal.1​6​7​8​ También fueron incluidos en
+                el movimiento conocido como nueva ola de heavy metal
+                americano.9​
+              </p>
+              <img
+                src=" https://firebasestorage.googleapis.com/v0/b/spotycaa.appspot.com/o/slpknot1.jpg?alt=media&token=7cd2f44f-77c5-4f74-90c0-b9338330d7ac"
+                class="img-fluid w-100"
+              ></img>
+            </div>
+            <div class="col-12 col-md-4">
+              <i class="bi bi-vinyl-fill fs-1">Medellin</i>
+              <div>8 diciembre 2022</div>
+              <i class="bi bi-vinyl-fill fs-1">Cali</i>
+              <div>10 diciembre 2022</div>
+              <i class="bi bi-vinyl-fill fs-1">Barranquilla</i>
+              <div>14 de diciembre 2022</div>
+              <i class="bi bi-vinyl-fill fs-1">Bogota</i>
+              <div>16 dicimebre 2022</div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-12 col-md-4">
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/spotycaa.appspot.com/o/slipknot2.jpg?alt=media&token=bf332eb4-8dee-432e-98b7-86e3de267a1c"
+                alt=""
+                class="img-fluid w-100"
+              ></img>
+            </div>
+            <div class="col-12 col-md-4">
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/spotycaa.appspot.com/o/slipknot5.jpg?alt=media&token=8d4a8b6a-9b99-48e4-8861-055560ea45f1"
+                alt=""
+                class="img-fluid w-100"
+              ></img>
+            </div>
+            <div class="col-12 col-md-4 ">
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/spotycaa.appspot.com/o/Slipknot3.jpg?alt=media&token=252699ec-fd49-4e21-b2b8-5ebe0dcc5054"
+                alt=""
+                class="img-fluid w-100"
+              ></img>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
